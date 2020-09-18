57173012249ba2af1d98bdca548ade316a85b1ce
joaodias <joaovitordias92040@gmail.com>
2020-09-18 17:16:41 -0300

    Arrumado a estilização das leis

--- src/Pages/Leis.js
+++ src/Pages/Leis.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import Card_Leis from '../components/Card_Leis/Card_Leis';
+import CardLeis from '../components/CardLeis/CardLeis';
 import { Container, Row, Col } from 'reactstrap';
 
 const leis = () => {
     return (
-        <Container>
-            <Row>
-                <Col sm="4">
-                    <Card_Leis
+        <Container  >
+            <Row className="mt-4">
+                <Col sm="4" md="4" className="mb-4">
+                    <CardLeis
                         title={"Lei Municipal nº 9.571"}
                         subtitle={"17 de dezembro de 1997"}
                         text={"Estabelecimentos são obrigados a ter à disposição de seus clientes e usuários portadores de deficiência visual, cardápios e relações de preços de seus serviços impressos em sistema de leitura 'Braile'."}
                     />
                 </Col>
-                <Col sm="4">
-                    <Card_Leis
+                <Col sm="4" md="4" className="mb-4">
+                    <CardLeis
                         title={"Lei Estadual nº 12.724"}
                         subtitle={"09 de novembro de 2007"}
                         text={"Dispõe sobre período mínimo de gratuidade em estacionamentos para veículos automotores utilizados por pessoas portadoras de necessidades especiais."}
                     />
                 </Col>
-                <Col sm="4">
-                    <Card_Leis
+                <Col sm="4" md="4" className="mb-4">
+                    <CardLeis
                         title={"Lei Estadual nº 12.724"}
                         subtitle={"09 de novembro de 2007"}
                         text={"Dispõe sobre período mínimo de gratuidade em estacionamentos para veículos automotores utilizados por pessoas portadoras de necessidades especiais."}
