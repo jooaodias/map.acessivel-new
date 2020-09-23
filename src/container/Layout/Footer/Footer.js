acff5e5b8c59ece7bcfe39dba7c3037a69d0b2a5
joaodias <joaovitordias92040@gmail.com>
2020-09-22 22:15:57 -0300

    Arrumado o footer

--- src/container/Layout/Footer/Footer.js
+++ src/container/Layout/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-5 py-1" style={{backgroundColor: "#4593EE"}}>
             <Container>
-                <p className="mt-5 mb-3" style={{color: "white",textAlign: "center"}}>© Ana Clara, João Vitor, Luiz Roberto</p>
+                <p className="mt-5 mb-3" style={{color: "white",textAlign: "center"}}>Copyright by Ana Clara, João Vitor, Luiz Roberto</p>
             </Container>
         </footer>
     )
