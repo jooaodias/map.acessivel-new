8fed493a9d94cf3ed80b3a6caabad27b42034121
joaodias <joaovitordias92040@gmail.com>
2020-10-30 13:15:12 -0300

    Inserindo leaflet e react-leaflet com alguns testes

--- src/Pages/Mapa.js
+++ src/Pages/Mapa.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import { Container } from 'reactstrap';
-import foto_ex from '../assets/images/aMap - Horizontal.png';
+import React, { useState } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import * as LocalData from '../components/Map/estabelecimentos.json';
+import { Icon } from 'leaflet';
 
-const Mapa = () =>{
-    return(
-        <Container>
-            Aqui terá um mapa
-            <img src={foto_ex} alt={"Foto do aMap"} width="100%"/>
-        </Container>
+const skater = new Icon({
+    iconUrl: '/logo.png',
+    iconSize: [30, 40]
+
+})
+
+function Mapa() {
+    const [activeLocal, setActiveLocal] = useState(null);
+    console.log(LocalData.features);
+    return (
+        <Map center={[-22.897475,-47.0682962]} zoom={14}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            {LocalData.features.map(local => (
+                <Marker
+                    key={local.properties.LOCAL_ID}
+                    position={[local.geometry.coordinates[1], local.geometry.coordinates[0]]}
+                    onClick={() => {
+                        setActiveLocal(local);
+                    }}
+                    icon={skater}
+
+                />
+            ))}
+            
+            {activeLocal && (
+                <Popup
+                    position={[
+                        activeLocal.geometry.coordinates[1],
+                        activeLocal.geometry.coordinates[0]
+                    ]}
+                    onClose={() => {
+                        setActiveLocal(null);
+                    }}
+                >
+                    <div>
+                        <h2>{activeLocal.properties.NAME}</h2>
+                        <p>{activeLocal.properties.DESCRIPTIO}</p>
+                    </div>
+
+                </Popup>
+            )}
+        </Map>
+
     )
 }
 
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Mapa = () =>{
     return(
         <div>
-
+            
         </div>
     )
 }
