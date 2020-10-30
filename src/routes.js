8351701192ae415d943d127c7bf968ec4e349d2c
joaodias <joaovitordias92040@gmail.com>
2020-10-29 22:49:12 -0300

    Arrumando routes

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
-import Mapa from './components/Map/Map';
+import Mapa from './pages/Mapa';
 
 class Routes extends Component{
     render(){
         let routes = (
             <Switch>
                 <Route exact path="/" component={LandingPage}/>
-                <Route exact path="/Mapa" component={Mapa}/>
+                <Route exact path="/mapa" component={Mapa}/>
 
                 <Redirect to="/"/>
             </Switch>
