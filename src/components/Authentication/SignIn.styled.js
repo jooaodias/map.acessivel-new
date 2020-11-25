1cd8cc312838d8c8c3d0cfdebb53d9d81d8f2088
joaodias <joaovitordias92040@gmail.com>
2020-11-24 23:38:40 -0300

    Estilização do Authentication

---
+++ src/components/Authentication/SignIn.styled.js
@@ -0,0 +1,36 @@
+import { Button, Input} from 'reactstrap';
+import styled from 'styled-components';
+
+
+export const Text = styled.p`
+margin-top: 5px;
+color: #4A619F;
+font-size: 16px;
+`
+
+export const InputStyled = styled(Input)`
+  top: 5px;
+  border-radius: 10px;
+  padding: 24px;
+  box-shadow: inset 0px 0px 2px rgba(0, 0, 0, 0.8);
+
+  ::placeholder,
+  ::-webkit-input-placeholder {
+      color: #E5E5E5;
+  }
+`;
+
+export const ButtonStyled = styled(Button)`
+    background-color: #4A619F;
+    color: white;
+    font-weight: 500;
+    border-radius: 15px;
+    cursor: pointer;
+    border: 2px solid #4A619F;
+    width: 100%; 
+
+    :hover{
+        background-color: #4593EE;
+        border: 2px solid white; 
+    }
+`
