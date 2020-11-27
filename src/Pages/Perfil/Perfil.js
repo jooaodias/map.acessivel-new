357d1f01decaf830c94976043be72dd390698bc9
joaodias <joaovitordias92040@gmail.com>
2020-11-26 23:51:30 -0300

    Criado Perfil

---
+++ src/Pages/Perfil/Perfil.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import { PerfilImage, ContainerProfile, PerfilName, PerfilTitle } from './Perfil.styled.js'
+
+import firebase from 'firebase/app';
+import styled, { keyframes } from 'styled-components';
+
+function Perfil() {
+    const [user, setUser] = useState(null);
+
+    const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                const currentUser = firebase.auth().currentUser;
+                setUser(currentUser);
+            }
+        });
+    }, []);
+    console.log(user)
+
+    const Rotate = styled.div`
+    display: inline-block;
+    animation: ${rotate} 2s linear infinite;
+    padding: 2rem 1rem;
+    font-size: 1.2rem;
+    `;
+
+    return (
+        <ContainerProfile className="py-4 d-flex flex-column">
+            <PerfilTitle>Suas Informações:</PerfilTitle>
+            <PerfilImage
+                src={user?.photoURL}
+                width="143px"
+                height="143px"
+                placeholderText="Foto"
+                className="border mx-auto"
+                alt={`Foto de ${user?.displayName}`}
+            />
+            <PerfilName>
+                Nome: {user?.displayName}
+            </PerfilName>
+            <PerfilName>
+                E-mail: {user?.email}
+            </PerfilName>
+            <div className="d-flex justify-content-center">
+            <Rotate>&lt; 😎 &gt;</Rotate>
+            </div>
+        </ContainerProfile>
+    )
+}
+
+export default Perfil
