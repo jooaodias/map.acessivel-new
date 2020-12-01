f467522134f30b6ea6ae3df1fad48225b78f5170
joaodias <joaovitordias92040@gmail.com>
2020-11-30 22:48:53 -0300

    Arrumando espaço

--- src/Pages/Maping.js
+++ src/Pages/Maping.js
@@ -3,12 +3,12 @@ import { ButtonStyled } from '../components/Places/Places.styled';
 import Mapa from '../components/Map/Map';
 
 function Maping() {
-    
+
     return (
         <>
-        <Mapa />
-        <ButtonStyled className="mt-4" href="/cadastro-local">Cadastre um Estabelecimento!</ButtonStyled>
-        <ButtonStyled className="mt-2" href="/lista-locais">Estabelecimentos Cadastrados</ButtonStyled>
+            <Mapa />
+            <ButtonStyled className="mt-4" href="/cadastro-local">Cadastre um Estabelecimento!</ButtonStyled>
+            <ButtonStyled className="mt-2" href="/lista-locais">Estabelecimentos Cadastrados</ButtonStyled>
         </>
 
     )
