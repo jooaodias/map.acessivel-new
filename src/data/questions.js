2208622e18f7c33b46959638f21570d96bc5265c
jooaodias <joaovitordias92040@gmail.com>
2021-11-16 15:59:17 -0300

    updating questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,16 +1,19 @@
 export const questions = [
+  //visual
   'Neste estabelecimento, há piso tátil em todo o espaço?',
   `Existem estruturas com sonorização ou equipamentos em Braile que tornam o espaço mais acessível para você?
     Ex: Elevadores, Cardápios, Alarmes
     `,
   'Há sinalização de atendimento prioritário a pessoas com deficiência?',
   'Além do piso tátil, existem objetos de apoio que auxiliam sua locomoção no espaço?',
+  //motor
   'Neste estabelecimento existem rampas de acesso para melhor locomoção?',
   'Nos banheiros, há cabines e sanitários acessíveis e bem equipados?',
   'O mobiliário e a recepção estão adaptados nesse local?',
   'Existe a disponibilidade de áreas especiais de embarque e desembarque (exterior e interior) ?',
+  //auditivo
   'Existem pessoas qualificadas para o atendimento, ou seja, que compreendem a linguagem de sinais e auxiliam o processo oferecido?',
-  'Há sinalização de atendimento prioritário a pessoas com deficiência?',
+  'Há sinalização de atendimento prioritário, ou ainda estruturas com iluminação, figuras e símbolos em LIBRAS para locais como banheiros, área de embarque e desembarque e alarmes, dentre outros itens?',
   'Existem estruturas digitais ou autoatendimento que contribuem para a acessibilidade do local?',
-  'Teste questão 12',
+  'Este espaço tem grande concentração de pessoas, o que pode acarretar em vibrações sonoras que atrapalham o modo de usufruir do local?',
 ];
